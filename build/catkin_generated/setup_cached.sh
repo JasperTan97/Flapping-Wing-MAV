35124dfae2817e31ac1c577e40737c6948f65498
JasperTan97 <jasper.tan@u.nus.edu>
2021-03-09 20:41:16 +0800

    first catkin_make

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH='/home/jasper/birb_ws/devel/lib:/opt/ros/melodic/lib'
+export PWD='/home/jasper/flap_ws/build'
